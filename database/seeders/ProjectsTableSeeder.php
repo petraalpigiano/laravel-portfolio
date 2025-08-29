c52834dc87240543f84b9e55f29723a871be325c
petraalpigiano <petraalpigiano@gmail.com>
2025-08-29 10:37:29 +0200

    add seeder with faker

---
+++ database/seeders/ProjectsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+
+class ProjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 5; $i++) {
+            $newProject = new Project();
+
+            $newProject->title = $faker->word();
+            $newProject->note = $faker->paragraph();
+
+
+            $newProject->save();
+        }
+    }
+}
