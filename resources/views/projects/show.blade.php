4697ee43c0d48f82a455c1fb5a8c9bed24418a2f
petraalpigiano <petraalpigiano@gmail.com>
2025-08-29 11:53:32 +0200

    add basic layout for show and login

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,1 +1,8 @@
+@extends('layouts.app')
 
+@section('show')
+    <div class="container my-4">
+        <h3>{{ $project['title'] }}</h3>
+        <p>{{ $project['note'] }}</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('projects', ProjectController::class);
+Route::resource('projects', ProjectController::class)->middleware(['auth', 'verified']);
 
 require __DIR__ . '/auth.php';
