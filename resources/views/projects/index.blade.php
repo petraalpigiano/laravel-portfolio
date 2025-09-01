5d4e147ade2f9cbb27ca12ab5d87538809af850e
petraalpigiano <petraalpigiano@gmail.com>
2025-09-01 16:25:49 +0200

    add buttoms

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -2,6 +2,12 @@
 
 @section('index')
     <div class="container my-4">
+        <div class="container d-flex space-between justify-content-between my-4 px-0">
+            <h2 class="fs-4 text-secondary my-0">
+                {{ __('Progetti') }}
+            </h2>
+            <button type="button" class="btn btn-outline-primary">Aggiungi</button>
+        </div>
         <div class="row row-cols-1 row-cols-md-3 g-4">
             @foreach ($projects as $project)
                 <div class="col">
@@ -16,6 +22,8 @@
                             <div class="my-2">
                                 <a class="btn btn-outline-primary" href={{ route('projects.show', $project) }}
                                     role="button">Vedi</a>
+                                <button type="button" class="btn btn-outline-info ">Modifica</button>
+                                <button type="button" class="btn btn-outline-danger">Cancella</button>
 
                             </div>
                         </div>
